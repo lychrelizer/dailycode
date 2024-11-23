cdce3f5e921fa85ed73261e9d8c5868edd440ec3
lychrelized <maeurer.alexander@gmail.com>
2024-11-23 12:35:04 +0100

    Ups

--- day-2/src/day-2.php
+++ day-2/src/day-2.php
@@ -1,7 +1,11 @@
 <?php
 
 function largestSwap($n) {
-    return ($n < 100) ? (int)(str_split($n, 1)[1].str_split($n, 1)[0]) >= $n : false;
+    return ($n < 100) ? (int)(str_split($n, 1)[1].str_split($n, 1)[0]) <= $n : false;
 }
 
-var_dump(largestSwap(100));
+var_dump(largestSwap(27));
+var_dump(largestSwap(43));
+var_dump(largestSwap(14));
+var_dump(largestSwap(53));
+var_dump(largestSwap(99));
