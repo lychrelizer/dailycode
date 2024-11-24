c79b430400b47a4b39f8e9e522fc44f4d69d6d24
lychrelized <maeurer.alexander@gmail.com>
2024-11-24 12:06:36 +0100

    Implement solution for Day 3

---
+++ day-3/src/day-3.php
@@ -0,0 +1,9 @@
+<?php
+
+function noOdds($a) {
+	return array_filter($a, fn($v) => !( $v & 1));
+}
+
+var_dump(noOdds([1, 2, 3, 4, 5, 6, 7, 8]));
+var_dump(noOdds([43, 65, 23, 89, 53, 9, 6]));
+var_dump(noOdds([718, 991, 449, 644, 380, 440]));
