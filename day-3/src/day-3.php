62bced57b7fce01610bce4694685c7c1ea96b6bb
lychrelized <maeurer.alexander@gmail.com>
2024-11-24 12:14:51 +0100

    Shorten Code

--- day-3/src/day-3.php
+++ day-3/src/day-3.php
@@ -1,8 +1,6 @@
 <?php
 
-function noOdds($a) {
-	return array_filter($a, fn($v) => !( $v & 1));
-}
+function noOdds($a){return array_filter($a,fn($v)=>!($v&1));}
 
 var_dump(noOdds([1, 2, 3, 4, 5, 6, 7, 8]));
 var_dump(noOdds([43, 65, 23, 89, 53, 9, 6]));
