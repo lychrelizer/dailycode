fda65a6a2c79fa65396dabda045cddc8cfee21d0
maeurer.alexander@gmail.com <maeurer.alexander@gmail.com>
2024-11-25 11:05:23 +0100

    First Implementation with lot of room for improvement

--- day-4/src/day-4.php
+++ day-4/src/day-4.php
@@ -1,5 +1,21 @@
 <?php
+//900uwu50uwuuwuuwu25uwu25
 
 function grabNumberSum($str) {
-	
+    $numbers = [];
+    $curnum = "";
+
+    foreach (str_split($str) as $c) {
+        if (is_numeric($c)) {
+            $curnum .= $c;
+        } else {
+            array_push($numbers, $curnum);
+            $curnum = ""; 
+        }
+    }
+
+    array_push($numbers, $curnum);
+    return array_sum($numbers);
 }
+
+var_dump(grabNumberSum("900uwu50uwuuwuuwu25uwu25"));
